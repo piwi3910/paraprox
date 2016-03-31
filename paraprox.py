0daf27feea56201130c414a25c2e603fb43ca44b
Alexander Logger <intagger@gmail.com>
2016-03-31 23:37:31 +0300

    Add detection of big files.
    Signed-off-by: Alexander Logger <intagger@gmail.com>

--- paraprox.py
+++ paraprox.py
@@ -1,17 +1,23 @@
 #!/usr/bin/python
 
+import os
 import sys
 from http import HTTPStatus
+from http.client import HTTPConnection, HTTPMessage, HTTPResponse, RemoteDisconnected
+from http.server import HTTPServer, BaseHTTPRequestHandler
 from socket import SocketIO, socket
 from socketserver import ThreadingMixIn
-from http.server import HTTPServer, BaseHTTPRequestHandler
-from http.client import HTTPConnection, HTTPMessage, HTTPResponse, RemoteDisconnected
 from typing import Tuple, Union, Optional
+from urllib.parse import urlparse, ParseResult
 
 DEFAULT_SERVER_ADDRESS = ('127.0.0.1', 8880)
 __version__ = "0.1"
 _UNKNOWN = 'UNKNOWN'
 _MAX_BUFFER_SIZE = 16 * 1024
+
+files_to_parallel = ['.iso', '.zip', '.rpm']
+file_chunk_length = 10 * 1024 * 1024
+max_parallel_downloads = 10
 
 
 class ParaproxHTTPRequestHandler(BaseHTTPRequestHandler):
@@ -49,6 +55,28 @@ class ParaproxHTTPRequestHandler(BaseHTTPRequestHandler):
             self.close_connection = True
             return
 
+    def is_file_to_parallel(self) -> bool:
+        pr = urlparse(self.path)  # type: ParseResult
+        path, ext = os.path.splitext(pr.path)
+        return ext.lower() in files_to_parallel
+
+    def get_file_info(self) -> Tuple[int, bool]:
+        """Make a HEAD request to get a 'content-length' and 'accept-ranges' headers."""
+        host_conn = HTTPConnection(self.host)
+        try:
+            host_conn.request('HEAD', self.path, headers=self.headers)
+            hr = host_conn.getresponse()
+            hrh = hr.headers  # type: HTTPMessage
+            accept_ranges = hrh.get('accept-ranges') == 'bytes'
+            file_length = hrh.get('content-length')
+            if file_length is not None:
+                file_length = int(file_length)
+            return file_length, accept_ranges
+        except (RemoteDisconnected, BrokenPipeError):
+            self.log_message('Disconnected.')
+        finally:
+            host_conn.close()
+
     def parse_request(self):
         if not super().parse_request():
             return False
@@ -64,7 +92,11 @@ class ParaproxHTTPRequestHandler(BaseHTTPRequestHandler):
 
         return True
 
-    def traverse_request(self):
+    def traverse_request_parallel(self, file_length: int):
+        self.log_message('Downloading file (%d bytes): `%s` in parallel...', file_length, self.path)
+        self.traverse_request_normal()  # TODO: implement.
+
+    def traverse_request_normal(self):
         body = None
         if isinstance(self.content_length, int) and self.content_length > 0:
             body = self.rfile  # We have a content to send, so set body as read file.
@@ -79,6 +111,14 @@ class ParaproxHTTPRequestHandler(BaseHTTPRequestHandler):
             self.log_message('Disconnected.')
         finally:
             host_conn.close()
+
+    def traverse_request(self):
+        if self.command == 'GET' and self.is_file_to_parallel():
+            file_length, accept_ranges = self.get_file_info()
+            if accept_ranges and file_length is not None and file_length > file_chunk_length:
+                self.traverse_request_parallel(file_length)
+        else:
+            self.traverse_request_normal()
 
     def traverse_response(self):
         self.traverse_response_headers()

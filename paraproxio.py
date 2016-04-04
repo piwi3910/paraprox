0efeb015f0b119c5ad731f5e366172f0cd594ff0
Alexander Logger <intagger@gmail.com>
2016-04-04 15:47:34 +0300

    Add normal logger. Fix bug with truncated content.
    Signed-off-by: Alexander Logger <intagger@gmail.com>

--- paraproxio.py
+++ paraproxio.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python
 
 import asyncio
+import logging
 import os
 import shutil
 import sys
-import time
 from asyncio import AbstractEventLoop
 from typing import Tuple, Callable, Optional, List
 from urllib.parse import urlparse
@@ -12,7 +12,6 @@ from urllib.parse import urlparse
 import aiohttp
 import aiohttp.hdrs as hdrs
 import aiohttp.server
-from aiohttp.log import server_logger, access_logger
 from aiohttp.multidict import CIMultiDictProxy
 from aiohttp.protocol import RawRequestMessage
 from aiohttp.streams import EmptyStreamReader
@@ -20,7 +19,14 @@ from aiohttp.streams import EmptyStreamReader
 DEFAULT_CHUNK_SIZE = 64 * 1024
 DEFAULT_PARALLELS = 10
 
-DEFAULT_BUFFER_DIR = '.paraprox_buffer'
+DEFAULT_WORKING_DIR = '.paraprox'
+DEFAULT_BUFFER_DIR = os.path.join(DEFAULT_WORKING_DIR, 'buffer')
+DEFAULT_LOGS_DIR = os.path.join(DEFAULT_WORKING_DIR, 'logs')
+DEFAULT_SERVER_LOG_FILENAME = 'paraprox.server.log'
+DEFAULT_ACCESS_LOG_FILENAME = 'paraprox.access.log'
+
+server_logger = logging.getLogger('paraprox.server')
+access_logger = logging.getLogger('paraprox.access')
 
 files_to_parallel = ['.iso', '.zip', '.rpm', '.gz']
 
@@ -219,21 +225,25 @@ class HttpRequestHandler(aiohttp.server.ServerHttpProtocol):
     def check_request(self, message: RawRequestMessage):
         if message.method == hdrs.METH_CONNECT:
             self.handle_error(status=405, message=message)
-            raise UnsupportedError('Method \'%s\' is not supported.' % message.method)
+            raise UnsupportedError("Method '%s' is not supported." % message.method)
 
     async def handle_request(self, message: RawRequestMessage, payload):
-        try:
-            self.check_request(message)
-            self.keep_alive(True)
-            if not await self.process_parallel(message, payload):
-                await self.process_normally(message, payload)
-        except ParaproxError as err:
-            request_repr = "{!s} {!r}".format(message.method, message.path)
-            self.log_message("Unable to {}. {!r}".format(request_repr, err))
+        now = self._loop.time()
+
+        self.check_request(message)
+        self.keep_alive(True)
+
+        # Try to process parallel.
+        response = await self.process_parallel(message, payload)
+
+        # Otherwise process normally.
+        if not response:
+            response = await self.process_normally(message, payload)
+
+        self.log_access(message, None, response, self._loop.time() - now)
 
     async def process_normally(self, message: RawRequestMessage, payload):
         """Process request normally."""
-        self.log_message("{!s} {!r}".format(message.method, message.path))  # TODO: integrate aiohttp logging.
         req_data = payload if not isinstance(payload, EmptyStreamReader) else None
 
         # Request from a host.
@@ -249,53 +259,52 @@ class HttpRequestHandler(aiohttp.server.ServerHttpProtocol):
                 for name, value in host_resp.headers.items():
                     if name == hdrs.CONTENT_ENCODING:
                         continue
+                    if name == hdrs.CONTENT_LENGTH:
+                        continue
                     if name == hdrs.TRANSFER_ENCODING:
                         if value.lower() == 'chunked':
                             client_res.enable_chunked_encoding()
                     client_res.add_header(name, value)
 
-                if client_res.length is None and client_res.headers.get(hdrs.TRANSFER_ENCODING) is None:
-                    client_res.autochunked = lambda: False
-
                 # Send headers to the client.
                 client_res.send_headers()
 
                 # Send a payload.
+                read = 0
                 while True:
                     chunk = await host_resp.content.read(DEFAULT_CHUNK_SIZE)
+                    read += len(chunk)
                     if not chunk:
                         break
                     client_res.write(chunk)
 
                 if client_res.chunked or client_res.autochunked():
                     client_res.write_eof()
-
-        except (aiohttp.ServerDisconnectedError, aiohttp.ClientResponseError):
-            self.log_message("Connection error.")
         finally:
             session.close()
+        return client_res
 
-    async def process_parallel(self, message: RawRequestMessage, payload) -> bool:
+    async def process_parallel(self, message: RawRequestMessage, payload) -> aiohttp.Response:
         """Try process a request parallel. Returns True in case of processed parallel, otherwise False."""
         # Checking the opportunity of parallel downloading.
-        if message.method != hdrs.METH_HEAD or not need_file_to_parallel(message.path):
-            return False
+        if message.method != hdrs.METH_GET or not need_file_to_parallel(message.path):
+            return None
         head = await self.get_file_head(message.path)
         if head is None:
-            return False
+            return None
         accept_ranges = head.get(hdrs.ACCEPT_RANGES).lower() == 'bytes'
         if not accept_ranges:
-            return False
+            return None
         content_length = head.get(hdrs.CONTENT_LENGTH)
         if content_length is None:
-            return False
+            return None
         content_length = int(content_length)
         if content_length <= 0:
-            return False
+            return None
 
         # All checks pass, start a parallel downloading.
-        # TODO: integrate aiohttp logging.
-        self.log_message('PARALLEL GET %s [%s bytes]' % (message.path, content_length))
+        # TODO: log as access, not as debug.
+        self.log_debug('PARALLEL GET %s [%s bytes]' % (message.path, content_length))
 
         # Get additional file info.
         content_type = head.get(hdrs.CONTENT_TYPE)
@@ -314,7 +323,7 @@ class HttpRequestHandler(aiohttp.server.ServerHttpProtocol):
         finally:
             await pd.clear()
 
-        return True
+        return client_res
 
     async def get_file_head(self, path: str) -> Optional[CIMultiDictProxy]:
         """Make a HEAD request to get a 'content-length' and 'accept-ranges' headers."""
@@ -323,49 +332,14 @@ class HttpRequestHandler(aiohttp.server.ServerHttpProtocol):
             async with session.request(hdrs.METH_HEAD, path) as res:  # type: aiohttp.ClientResponse
                 return res.headers
         except (aiohttp.ServerDisconnectedError, aiohttp.ClientResponseError):
-            self.log_message("Connection error.")
+            self.log_debug("Could not get a HEAD for the {!r}.".format(path))
         finally:
             session.close()
         return None
 
-    def log_message(self, mformat, *args):
-        """Log an arbitrary message.
-
-        This is used by all other logging functions.  Override
-        it if you have specific logging wishes.
-
-        The first argument, FORMAT, is a format string for the
-        message to be logged.  If the format string contains
-        any % escapes requiring parameters, they should be
-        specified as subsequent arguments (it's just like
-        printf!).
-
-        The client ip and current date/time are prefixed to
-        every message.
-
-        """
-        sys.stderr.write("%s - - [%s] %s\n" %
-                         (self.get_client_address(),
-                          self.get_log_date_time_string(),
-                          mformat % args))
-
     def get_client_address(self):
         address, port = self.transport.get_extra_info('peername')
         return '%s:%s' % (address, port)
-
-    def get_log_date_time_string(self):
-        """Return the current time formatted for logging."""
-        now = time.time()
-        year, month, day, hh, mm, ss, x, y, z = time.localtime(now)
-        s = "%02d/%3s/%04d %02d:%02d:%02d" % (
-            day, self.monthname[month], year, hh, mm, ss)
-        return s
-
-    weekdayname = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
-
-    monthname = [None,
-                 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
-                 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
 
 
 class ParaproxError(Exception):
@@ -380,11 +354,49 @@ class UnsupportedError(ParaproxError):
     pass
 
 
+def setup_dirs(buffer_dir: str, logs_dir: str):
+    os.makedirs(buffer_dir, exist_ok=True)
+    os.makedirs(logs_dir, exist_ok=True)
+
+
+def setup_logging(logs_dir: str, server_log_filename: str, access_log_filename: str):
+    server_logger.setLevel(logging.DEBUG)
+    access_logger.setLevel(logging.DEBUG)
+
+    # stderr handler.
+    ch = logging.StreamHandler(sys.stderr)
+    ch.setLevel(logging.DEBUG)
+    server_logger.addHandler(ch)
+    access_logger.addHandler(ch)
+
+    # Server log file handler.
+    slfp = os.path.join(logs_dir, server_log_filename)
+    slfh = logging.FileHandler(slfp)
+    slfh.setLevel(logging.DEBUG)
+    server_logger.addHandler(slfh)
+
+    # Access log file handler.
+    alfp = os.path.join(logs_dir, access_log_filename)
+    alfh = logging.FileHandler(alfp)
+    alfh.setLevel(logging.DEBUG)
+    access_logger.addHandler(alfh)
+
+
 def run():
+    setup_dirs(DEFAULT_BUFFER_DIR, DEFAULT_LOGS_DIR)
+    setup_logging(DEFAULT_LOGS_DIR, DEFAULT_SERVER_LOG_FILENAME, DEFAULT_ACCESS_LOG_FILENAME)
     loop = asyncio.get_event_loop()
-    f = loop.create_server(
-        lambda: HttpRequestHandler(loop=loop, debug=True, keep_alive=75),
-        '127.0.0.1', '8880')
+
+    def create_http_request_handler():
+        return HttpRequestHandler(
+            loop=loop,
+            logger=server_logger,
+            access_log=access_logger,
+            debug=True,
+            keep_alive=75)
+
+    f = loop.create_server(create_http_request_handler, '127.0.0.1', '8880')
+
     srv = loop.run_until_complete(f)
     print('serving on', srv.sockets[0].getsockname())
     try:
